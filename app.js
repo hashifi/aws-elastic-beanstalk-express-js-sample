0ac7df476fb5a5dcf08354eedfe40d549b4c9dcd
hashifi <hashifi.fst.isa@gmail.com>
2021-02-12 23:47:09 +0800

    New messages

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hello Hashifi!'));
+app.get('/', (req, res) => res.send('Testing new pipeline!'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
